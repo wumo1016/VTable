d7e7eb574c9b6a00c1bfaf8dd1efd507640407f6
wumo1016 <760478279@qq.com>
2025-10-09 16:11:35 +0800

    fix: rightFrozenColCount

--- packages/vtable/examples/list/list.ts
+++ packages/vtable/examples/list/list.ts
@@ -1,202 +1,41 @@
-import * as VTable from '../../src';
+import { ListTable } from '../../src';
 
-import { InputEditor, DateInputEditor } from "@visactor/vtable-editors";
-
-VTable.register.editor("input-editor", new InputEditor());
-VTable.register.editor("date-input-editor", new DateInputEditor());
-
-function generateRandomString(length) {
-  let result = "";
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
-function generateRandomHobbies() {
-  const hobbies = [
-    "Reading books",
-    "Playing video games",
-    "Watching movies",
-    "Cooking",
-    "Hiking",
-    "Traveling",
-    "Photography",
-    "Playing \n musical /n instruments",
-    "Gardening",
-    "Painting",
-    "Writing",
-    "Swimming",
-  ];
-
-  const numHobbies = Math.floor(Math.random() * 3) + 1; // 生成 1-3 之间的随机整数
-  const selectedHobbies = [];
-
-  for (let i = 0; i < numHobbies; i++) {
-    const randomIndex = Math.floor(Math.random() * hobbies.length);
-    const hobby = hobbies[randomIndex];
-    selectedHobbies.push(hobby);
-    hobbies.splice(randomIndex, 1); // 确保每个爱好只选一次
-  }
-
-  return selectedHobbies.join(", ");
-}
-function generateRandomBirthday() {
-  const start = new Date("1970-01-01");
-  const end = new Date("2000-12-31");
-  const randomDate = new Date(
-    start.getTime() + Math.random() * (end.getTime() - start.getTime())
-  );
-  const year = randomDate.getFullYear();
-  const month = randomDate.getMonth() + 1;
-  const day = randomDate.getDate();
-  return `${year}-${month < 10 ? "0" + month : month}-${
-    day < 10 ? "0" + day : day
-  }`;
-}
-function generateRandomPhoneNumber() {
-  const areaCode = [
-    "130",
-    "131",
-    "132",
-    "133",
-    "134",
-    "135",
-    "136",
-    "137",
-    "138",
-    "139",
-    "150",
-    "151",
-    "152",
-    "153",
-    "155",
-    "156",
-    "157",
-    "158",
-    "159",
-    "170",
-    "176",
-    "177",
-    "178",
-    "180",
-    "181",
-    "182",
-    "183",
-    "184",
-    "185",
-    "186",
-    "187",
-    "188",
-    "189",
-  ];
-  const prefix = areaCode[Math.floor(Math.random() * areaCode.length)];
-  const suffix = String(Math.random()).substr(2, 8);
-  return prefix + suffix;
-}
-const generatePersons = (count) => {
-  return Array.from(new Array(count)).map((_, i) => {
-    const first = generateRandomString(10);
-    const last = generateRandomString(4);
-    return {
-      id: i + 1,
-      email1: `${first}_${last}@xxx.com`,
-      name: first,
-      lastName: last,
-      hobbies: generateRandomHobbies(),
-      birthday: generateRandomBirthday(),
-      // tel: generateRandomPhoneNumber(),
-      tel: i + 1,
-      address: `No.${i + 100} ${generateRandomString(
-        10
-      )} ${generateRandomString(5)}\n${generateRandomString(5)}`,
-      sex: i % 2 === 0 ? "boy" : "girl",
-      work: i % 2 === 0 ? "back-end engineer" : "front-end engineer",
-      city: "beijing",
+const container = document.getElementById('vTable');
+fetch('https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/North_American_Superstore_Pivot_data.json')
+  .then(res => res.json())
+  .then(data => {
+    const option = {
+      records: data,
+      columns: [
+        {
+          field: 'Category',
+          title: 'Category',
+          width: 400
+        },
+        {
+          field: 'City',
+          title: 'City',
+          width: 500
+        },
+        {
+          field: 'Quantity',
+          title: 'Quantity',
+          width: 200
+        },
+        {
+          field: 'Sales',
+          title: 'Sales',
+          width: 200
+        },
+        {
+          field: 'Profit',
+          title: 'Profit',
+          width: 200
+        }
+      ],
+      // frozenColCount: 2
+      rightFrozenColCount: 2
     };
+    const tableInstance = new ListTable(container, option);
+    window['tableInstance'] = tableInstance;
   });
-};
-const records = generatePersons(100);
-const columns = [
-  {
-    field: "id",
-    title: "ID",
-    width: 80,
-    sort: true,
-  },
-  {
-    field: "full name",
-    title: "Full name",
-    columns: [
-      {
-        field: "name",
-        title: "First Name\n(input editor)",
-        width: 120,
-        editor: "input-editor",
-      },
-      {
-        field: "lastName",
-        title: "Last Name\n(input editor)",
-        width: 100,
-      },
-    ],
-  },
-  {
-    field: "birthday",
-    title: "birthday\n(date editor)",
-    width: 120,
-    editor: (args) => {
-      console.log(args.row);
-      if (args.row === 2) {
-        return false;
-      }
-      return "input-editor";
-    },
-  },
-  {
-    field: "sex",
-    title: "sex\n(list editor)",
-    width: 100,
-  },
-  {
-    field: "address",
-    title: "address\n(textArea editor)",
-    width: 300,
-    editor: "textArea-editor",
-  },
-  {
-    field: "tel",
-    title: "telephone",
-    width: 150,
-    editor: "input-editor",
-  },
-  {
-    field: "work",
-    title: "job",
-    width: 200,
-  },
-  // {
-  //   field: "city",
-  //   title: "city",
-  //   width: 150,
-  // },
-];
-
-const option = {
-  container: document.getElementById("vTable"),
-  records,
-  columns,
-  enableLineBreak: true,
-  autoWrapText: true,
-  limitMaxAutoWidth: 600,
-  heightMode: "autoHeight",
-  editCellTrigger: "click",
-  keyboardOptions: {
-    copySelected: true,
-    pasteValueToCell: true,
-    selectAllOnCtrlA: true,
-  },
-  frozenRowCount: 3,
-};
-const tableInstance = new VTable.ListTable(option);
-window["tableInstance"] = tableInstance;
